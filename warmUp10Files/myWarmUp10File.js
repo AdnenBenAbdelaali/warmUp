d280f9e904b3b7219d38e580adb6c8330a049b2c
AdnenBenAbdelaali <benabdelaaliadnen@gmail.com>
2019-10-30 07:40:04 +0100

    warmup10 REFACTORED

--- warmUp10Files/myWarmUp10File.js
+++ warmUp10Files/myWarmUp10File.js
@@ -1,1 +1,35 @@
-$on("#firstButton").
+
+$('document').ready(function() {
+	$('body#dd').append('<ul></ul>');
+	var randClass = 0;
+	$('button#firstButton').click(function() {
+	var input = $('#text').val();
+	alert(input);
+	
+	
+	var theId = "liId" + randClass;
+	alert("the id is :  " + theId)
+	$('ul').append("<li class='' id=" + theId +"></li>")
+	var theIdDisplay = "#" + theId;
+	if(randClass % 2 === 0 ) {
+		$(theIdDisplay).attr("class","classOne");
+	}else if(randClass % 2 !== 0) {
+		$(theIdDisplay).attr("class","classTwo");
+	}
+	randClass++;
+	$(theIdDisplay).text(input);
+
+});
+
+	
+	
+	
+
+	
+
+
+	
+
+
+});
+
