59dab0976cc4309e449d60c267d07001de5aec67
AdnenBenAbdelaali <benabdelaaliadnen@gmail.com>
2019-10-31 08:14:52 +0100

    warmup11 refactored

--- warmup11files/myjs.js
+++ warmup11files/myjs.js
@@ -4,6 +4,50 @@
  // unordered list if the “right” button was pressed 
   //before the “create” button 
 /* left <ul></ul> */
-  $('#leftButton').on('click',function() {
-  	$('ul#left').append('<li></li>');
-  })
+
+	$('#createButton').click(function() {
+	var $left = false;
+	var $right = false;
+	var $clicker = true;
+	alert('clicker = ' + $clicker + '\n' + "left = " + $left + '\n' + "right = " + $right);
+
+
+$('#rightButton').click(function() {
+  $right = true;
+   alert('right = ' + $right);
+
+    if($clicker === true && $right === true) {
+			$('#right').append('<li>right button clicked</li>')
+			$clciker = false;
+			$right = false;	
+			
+		}
+	alert('clicker = ' + $clicker + '\n' + "left = " + $left + '\n' + "right = " + $right);
+	})
+		
+$('#leftButton').click(function() {		
+  $left = true;
+  alert('left = ' + $left);
+
+  if($clicker === true && $left === true) {
+		$('ul#left').append('<li>left button clicked</li>');
+		$clciker = false;
+		$left = false;
+	}
+
+	alert('clicker = ' + $clicker + '\n' + "left = " + $left + '\n' + "right = " + $right);
+	
+  }) 
+	
+
+	})
+
+		
+
+
+		
+
+
+
+
+
