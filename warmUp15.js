a0d0a2a30a64204b018f9cf93e94f46003480701
insafmechergui <insaf_mechergui@outlook.com>
2019-11-05 09:56:33 +0100

    my commit

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,34 @@
 //
 //
 // Note : you can`t use any built-in functions .
+function reverseString(str) {
+	var arr = [];
+	var firstWord = "";
+	var i = 0;
+	var h = 0;
+	var stop = 0;
+	while(i < str.length) {
+		
+		if(str[i + 1] === " ") {
+			if(arr.length === 0 ) {
+				var start = 0;
+			}else {
+				var start = i + 1;
+				var stop = start;
+				while(str[stop + 1] !== " ") {
+					stop++ ;
+				}
+			}
+			for(let j = start; j < stop; j++) {
+				firstWord += str[j];
+			
+			}
+			// var startCount = i + 2;
+            arr[h]= firstWord;
+			h++;
+		}
+		i++;
+	
+	}
+	return arr;
+}
